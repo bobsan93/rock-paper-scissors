14ea58abe5b7c10629fb7a956ecacdef3806578a
bobsan93 <johan.bobby@hotmail.se>
2022-12-05 13:36:12 +0100

    game function that does that game ones added

--- javasc.js
+++ javasc.js
@@ -4,21 +4,47 @@ let options = ['Rock', 'Paper', 'Scissors'];
 // computer will pick randome and display it;
 function getCumputerChoice () {
     let randomOptions = options[Math.floor(Math.random() * options.length)];
-    return randomOptions
+    let option = randomOptions.toString();
+    return option
 }    
 //player will pick a option
 function playerChoice (x) {
     x = prompt("Make your choice")
     if (x === "rock" || x === "Rock" || x === "ROCK") {
-        return 'Rock'
+        return "Rock"
     }else if (x === "paper" || x === "Paper" || x === "PAPER") {
-        return 'Paper'
+        return "Paper"
     }else if ( x === "scissors" || x === "Scissors" || x === "SCISSORS") {
-        return 'Scissors'
+        return "Scissors"
+    }
+
+
+}   
+function game (player, computer) {
+    player = playerChoice();
+    computer = getCumputerChoice();
+    let playerPoints = "Player Wins! ";
+    let computerPoints = "Computer wins! ";
+    const rockBeats = "Rock beats Scissors";
+    const paperBeats = "Paper beats Rock";
+    const scissorsBeats = "Scissors beats Paper"
+    if (player === "Rock" && computer === "Scissors") {
+        console.log(`${playerPoints}${rockBeats}`)
+    }else if (player ==="Rock" && computer === "Paper") {
+        console.log(`${computerPoints}${paperBeats}`)
+    }else if (player === "Paper" && computer === "Rock") {
+        console.log(`${playerPoints}${paperBeats}`)
+    }else if (player === "Paper" && computer === "Scissors") {
+        console.log(`${computerPoints}${scissorsBeats}`)
+    }else if (player === "Scissors" && computer === "Paper") {
+        console.log(`${playerPoints}${scissorsBeats}`)
+    } else if ( player === "Scissors" && computer == "Rock") {
+        console.log(`${computerPoints}${rockBeats}`)
+    }else {
+        console.log("Draw")
     }
     
-}    
-function game () {
-
+    
 }
 
+game ()
