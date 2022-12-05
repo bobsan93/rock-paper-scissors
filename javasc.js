751fdc5330be9e87a2a74fa672c7130ea7757499
bobsan93 <johan.bobby@hotmail.se>
2022-12-05 11:03:32 +0100

    function for playerChoice added

--- javasc.js
+++ javasc.js
@@ -7,3 +7,18 @@ function getCumputerChoice () {
     return randomOptions
 }    
 //player will pick a option
+function playerChoice (x) {
+    x = prompt("Make your choice")
+    if (x === "rock" || x === "Rock" || x === "ROCK") {
+        return 'Rock'
+    }else if (x === "paper" || x === "Paper" || x === "PAPER") {
+        return 'Paper'
+    }else if ( x === "scissors" || x === "Scissors" || x === "SCISSORS") {
+        return 'Scissors'
+    }
+    
+}    
+function game () {
+
+}
+
