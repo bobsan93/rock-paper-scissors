d297155fe7674e3f50ddf393e4789a061648077a
bobsan93 <johan.bobby@hotmail.se>
2022-12-06 18:51:26 +0100

    change scissors to scissor, score system working in gameloop, and reports the winer in the end

--- javasc.js
+++ javasc.js
@@ -1,11 +1,11 @@
-let options = ['Rock', 'Paper', 'Scissors'];
+let options = ['Rock', 'Paper', 'Scissor'];
 let playerScore = 0;
 let computerScore = 0;
 let playerWin = "Player Wins! ";
 let computerWin = "Computer Wins! ";
-const rockBeats = "Rock beats Scissors";
+const rockBeats = "Rock beats Scissor";
 const paperBeats = "Paper beats Rock";
-const scissorsBeats = "Scissors beats Paper";
+const scissorsBeats = "Scissor beats Paper";
 
 function getCumputerChoice () {
     let randomOptions = options[Math.floor(Math.random() * options.length)];
@@ -14,30 +14,30 @@ function getCumputerChoice () {
 }    
 
 function playerChoice () {
-    let choice1 = prompt("Make your choice")
+    let choice1 = prompt("Rock Paper or Scissor?")
     let choiceLower = choice1.toLocaleLowerCase()
     if (choiceLower === "rock") {
         return "Rock"
     } else if (choiceLower === "paper") {
         return "Paper"
-    } else if (choiceLower === "scissors") {
-        return "Scissors"
+    } else if (choiceLower === "scissor") {
+        return "Scissor"
     }
 }   
 function game (player, computer) {
     player = playerChoice();
     computer = getCumputerChoice();
-    if (player === "Rock" && computer === "Scissors") {
+    if (player === "Rock" && computer === "Scissor") {
         return `${playerWin}${rockBeats}`
     }else if (player ==="Rock" && computer === "Paper") {
         return `${computerWin}${paperBeats}`
     }else if (player === "Paper" && computer === "Rock") {
         return `${playerWin}${paperBeats}`
-    }else if (player === "Paper" && computer === "Scissors") {
+    }else if (player === "Paper" && computer === "Scissor") {
         return `${computerWin}${scissorsBeats}`
-    }else if (player === "Scissors" && computer === "Paper") {
+    }else if (player === "Scissor" && computer === "Paper") {
         return `${playerWin}${scissorsBeats}`
-    } else if ( player === "Scissors" && computer == "Rock") {
+    } else if ( player === "Scissor" && computer == "Rock") {
         return `${computerWin}${rockBeats}`
     }else {
         return "Draw"
@@ -46,16 +46,22 @@ function game (player, computer) {
 function gameLoop(winer) {    
     for (let i = 0; i < 5; i++){
         winer = game()
-        if (winer === "Player Wins! Rock beats Scissors" || winer === "Player Wins! Paper beats Rock" || winer === "Player Wins! Scissors beats Paper") {
-            
-            console.log(`${playerWin}`)
-        }else if (winer === "Computer Wins! Rock beats Scissors" || winer === "Computer Wins! Paper beats Rock" || winer === "Computer Wins! Scissors beats Paper") {
-            
-            console.log(`${computerWin}`)
+        if (winer === "Player Wins! Rock beats Scissor" || winer === "Player Wins! Paper beats Rock" || winer === "Player Wins! Scissor beats Paper") {
+            playerScore++
+            console.log(`${winer} Score ${playerScore}`)
+        }else if (winer === "Computer Wins! Rock beats Scissor" || winer === "Computer Wins! Paper beats Rock" || winer === "Computer Wins! Scissor beats Paper") {
+            computerScore++
+            console.log(`${winer} Score ${computerScore}`)
         }else {
             console.log("Draw! No points!")
         }
-        
+    }
+    if (playerScore > computerScore) {
+        console.log("Player is the winer!")
+    }else if (playerScore < computerScore) {
+        console.log("Computer is the winer!")
+    }else {
+        console.log("Its a draw! No winer!")
     }
 }    
 gameLoop()
